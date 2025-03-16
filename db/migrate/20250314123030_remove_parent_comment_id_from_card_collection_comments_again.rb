ffe076b6b5d004702505aa3b26d0bef17dc0b724
EC2 Default User <bjking102409@gmail.com>
2025-03-16 12:03:32 +0000

    migrateionFileRemove

--- db/migrate/20250314123030_remove_parent_comment_id_from_card_collection_comments_again.rb
+++
@@ -1,6 +0,0 @@
-class RemoveParentCommentIdFromCardCollectionCommentsAgain < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :card_collection_comments, :parent_comment_id if index_exists?(:card_collection_comments, :parent_comment_id)
-    remove_column :card_collection_comments, :parent_comment_id, :integer
-  end
-end
