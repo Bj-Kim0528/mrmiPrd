694ce13248b79ae060c7f3b1b8e5b8850f693df3
EC2 Default User <bjking102409@gmail.com>
2025-03-16 12:37:44 +0000

    resetPassword001

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
 
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '54.249.33.89', port: 'http' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
