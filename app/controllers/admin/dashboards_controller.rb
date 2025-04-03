1066921e398f9a7e0f6212084c86e7f8db9507ec
EC2 Default User <bjking102409@gmail.com>
2025-04-03 07:37:40 +0000

    Admin003

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,17 +1,7 @@
 class Admin::DashboardsController < ApplicationController
   layout 'admin'
-  before_action :require_admin
   before_action :authenticate_admin!
   def index
       @users = User.all
-  end
-
-
-  private
-
-  def require_admin
-    unless current_user && current_user.admin?
-      redirect_to root_path, alert: "관리자만 접근할 수 있습니다."
-    end
   end
 end
