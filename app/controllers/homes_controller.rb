c98d2558bce6be4141a14485dd81ba715fa40755
EC2 Default User <bjking102409@gmail.com>
2025-04-09 07:14:06 +0000

    Layout009

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,17 @@
 class HomesController < ApplicationController
   def top
+    @room_photo_theme = Theme.find_by(name: "お部屋写真")
+    @card_collections_room_photo = CardCollection.where(theme_id: @room_photo_theme.id).sort_by do |cc|
+      total_interactions = cc.likes.count +
+                           cc.bookmarks.count +
+                           cc.card_collection_comments.where(deleted: false).count +
+                           CardCollectionReply.joins(:card_collection_comment)
+                                              .where(card_collection_comments: { card_collection_id: cc.id }, deleted: false)
+                                              .count +
+                           cc.view_count
+      -total_interactions  # 내림차순 정렬을 위해 음수값 반환
+    end
+    @themes = Theme.where.not(name: ["お部屋写真", "その他"])
   end
 
   def about
