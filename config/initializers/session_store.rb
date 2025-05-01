fca1cdf281267b4a7a8e50a222d7631cea9c59b7
EC2 Default User <bjking102409@gmail.com>
2025-05-01 06:27:08 +0000

    CI/CD004

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -8,6 +8,6 @@
 
   Rails.application.config.session_store :cookie_store,
   key: '_myapp_session',
-  expire_after: 25.minute,       # ← 1분으로 설정
+  expire_after: 30.minute,       # ← 1분으로 설정
   secure: Rails.env.production?,
   same_site: :lax
