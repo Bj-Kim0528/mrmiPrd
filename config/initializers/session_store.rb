7ce811c05de4faddad6a4692ee1f0dcc4c421f03
EC2 Default User <bjking102409@gmail.com>
2025-05-01 05:56:16 +0000

    session002

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,13 @@
+# Rails.application.config.session_store :cookie_store,
+#   key: '_mrmi_session',        # ← 여러분 앱에 맞게 고쳐주세요
+#   expire_after: nil,           # 개발 중엔 nil, 운영에선 필요 시 분기 처리
+#   secure: Rails.env.production?,
+#   same_site: :lax
 
-Rails.application.config.session_store :cookie_store,
+
+
+  Rails.application.config.session_store :cookie_store,
   key: '_myapp_session',
-  expire_after: 1.minute,       # ← 1분으로 설정
+  expire_after: 30.minute,       # ← 1분으로 설정
   secure: Rails.env.production?,
   same_site: :lax
